be4be2a67c0d4c2733ef7a75fdbd5e6e79ae8095
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-07 16:06:11 +0530

    feat(auth):Implement a registration logic in auth services.

---
+++ src/models/Auth/Auth.services.js
@@ -0,0 +1,26 @@
+const AuthRepositories = require('./Auth.repositories')
+const bcryptSevices = require('../../utils/bcrypt')
+
+const AuthServices = {
+    register:async(payloads)=>{
+        const data = payloads
+        if(!data.email) throw new Error("Email is not defined");
+        if(!data.name) throw new Error("Name is not defined");
+        if(!data.password) throw new Error("Password is not defined");
+        
+        const existed =  await AuthRepositories.findbyemail(data.email)
+        
+        if(existed) throw new Error("Email is already existed");
+
+        const hashedpassword = await bcryptSevices.hashPassword(data.password)
+        
+        
+        AuthRepositories.createuser(data.name,data.email,hashedpassword)
+        
+        return{
+            message:"User Registerd succefully",
+            }
+    },
+}
+
+module.exports = AuthServices
