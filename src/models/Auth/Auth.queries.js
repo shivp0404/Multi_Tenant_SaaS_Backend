0bd844ac298972db72b3b03e26c4666599f2dbe3
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-07 21:31:31 +0530

    fix(auth): add RETURNING * to createuser in AuthQueries

--- src/models/Auth/Auth.queries.js
+++ src/models/Auth/Auth.queries.js
@@ -1,7 +1,7 @@
 
 
 const AuthQueries = {
- createuser: `INSERT INTO users(name,email,password)VALUES($1,$2,$3)`,
+ createuser: `INSERT INTO users(name,email,password)VALUES($1,$2,$3) RETURNING *`,
  findbyemail:`SELECT * FROM users WHERE email = $1`,
 }
 
