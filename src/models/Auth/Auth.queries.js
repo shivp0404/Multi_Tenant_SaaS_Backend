78d483c3ea2aa481356e284ebe16557b35232666
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-10 11:24:44 +0530

    feat(auth): add a saveRefreshToken in AuthRepositories.

--- src/models/Auth/Auth.queries.js
+++ src/models/Auth/Auth.queries.js
@@ -3,6 +3,7 @@
 const AuthQueries = {
  createuser: `INSERT INTO users(name,email,password)VALUES($1,$2,$3) RETURNING *`,
  findbyemail:`SELECT * FROM users WHERE email = $1`,
+ saveRefreshToken:`UPDATE users SET refreshtoken = $1 WHERE id = $2`
 }
 
 module.exports = AuthQueries
