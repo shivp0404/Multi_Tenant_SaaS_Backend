5568dc5c165bf916373029e515033c0cb50b1a3e
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-16 12:35:08 +0530

    feat(tenant):add a controller invite for invitation

--- src/models/tenant/tenantController.js
+++ src/models/tenant/tenantController.js
@@ -13,6 +13,7 @@ const TenantsControllers = {
       .json(ApiResponse.success("Tenant Created",response));
   },
   mytenants:async(req,res)=>{
+    console.log(req.user.id)
     const response =  await tenantService.mytenants(
       req.user.id
     )
@@ -20,6 +21,12 @@ const TenantsControllers = {
 
     }
   ,
+ invite:async(req,res)=>{
+  const{email,role} = req.body
+  const tenantId = req.params.id
+  const response = await tenantService.inviteUser(tenantId,email,role);
+  res.status(200).json(ApiResponse.success("Send Invitation",response))
+ }
 
 
 };
