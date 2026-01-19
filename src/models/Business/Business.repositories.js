86b0d7c515afde0cbbc08514780be5ec848a5898
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-19 12:23:12 +0530

    feat(business):add a repositorie for creating task

---
+++ src/models/Business/Business.repositories.js
@@ -0,0 +1,14 @@
+const BusinessQueries = require("./Business.queries")
+const pool = require("../../../config/db")
+const BusinessRepository = {
+    createTask: async(tenantid,title,description,status,priority,assigned_to,created_by,updated_by)=>{
+        const result = await pool.query(BusinessQueries.createTask,[tenantid,title,description,status,priority,assigned_to,created_by])
+        return result.rows
+    },
+    validateAssignUser: async(tenantid,userid)=>{
+        const result = await pool.query(BusinessQueries.validateAssignUser,[tenantid,userid])
+        return result.rows
+    }
+}
+
+module.exports = BusinessRepository
