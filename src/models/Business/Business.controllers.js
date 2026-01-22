ba2f12f4c4f1698a0e48b65d689e42d5821ee4b4
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-22 11:55:45 +0530

    feat(business):add a controller for fetchtask and deletetask

--- src/models/Business/Business.controllers.js
+++ src/models/Business/Business.controllers.js
@@ -7,6 +7,19 @@ const BusinessControllers ={
         const userId = req.user.id
         const response = await BusineesService.createTask(tenantId,userId,data);
         res.status(201).json(ApiResponse.success("Task created",response))
+    },
+    fetchTask:async(req,res)=>{
+        const tenantId = req.params.id
+        const userid = req.user.id
+        const response = await BusineesService.fetchTask(userid,tenantId)
+        res.status(200).json(ApiResponse.success("All Task",response))
+    },
+    deleteTask:async(req,res)=>{
+        
+        const taskid = req.params.id2
+        const tenantid = req.params.id1
+        const response = await BusineesService.deleteTask(taskid,tenantid)
+        res.status(200).json(ApiResponse.success("Task Deleted",response))
     }
 }
 module.exports = BusinessControllers
