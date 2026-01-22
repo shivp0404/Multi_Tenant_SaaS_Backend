eb1d6230f747ea2e5c6de49b53e76a0a9ae10844
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-22 13:09:39 +0530

    feat(middleware):add a rbac middlewar in routes

--- src/routers/BusinessRouters.js
+++ src/routers/BusinessRouters.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const BusinessControllers = require('../models/Business/Business.controllers')
 const Authenticate = require("../middleware/Authenticate")
+const Authorization = require("../middleware/can")
 router.use(Authenticate)
 
-router.post('/:id/createtask',BusinessControllers.createTask)
-router.get('/:id/tasks',BusinessControllers.fetchTask)
-router.post('/:id1/:id2/deletetask',BusinessControllers.deleteTask)
+router.post('/:id/createtask',Authorization('task.create'),BusinessControllers.createTask)
+router.get('/:id/tasks',Authorization('task.view'),BusinessControllers.fetchTask)
+router.post('/:id1/:id2/deletetask',Authorization('task.view'),BusinessControllers.deleteTask)
 
 module.exports = router
--- src/routers/TenantRouters.js
+++ src/routers/TenantRouters.js
@@ -2,12 +2,13 @@ const express = require("express")
 const router = express.Router()
 const Authenticate = require("../middleware/Authenticate")
 const TenantsControllers = require("../models/tenant/tenantController")
+const Authorization = require("../middleware/can")
 router.use(Authenticate)
 
 router.post('/create',TenantsControllers.createTenant);
 router.get('/me',TenantsControllers.mytenants);
 router.post('/:id/invite',TenantsControllers.invite);
-router.get('/invitations',TenantsControllers.invitations)
+router.get('/invitations',Authorization('tenant.invite'),TenantsControllers.invitations)
 router.post('/invitations/:id/accept',TenantsControllers.AcceptInvitations)
 router.post('/invitations/:id/reject',TenantsControllers.RejectInvitations)
 
