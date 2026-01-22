47a87e859341c073c5918ab7784e33be722d51ab
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-22 11:46:35 +0530

    feat(business):add a service for fetchtask and deletetask
    

--- src/models/Business/Business.service.js
+++ src/models/Business/Business.service.js
@@ -1,6 +1,7 @@
 const BusinessRepository = require("./Business.repositories")
 const AppError = require("../../utils/AppError")
-const AuthRepository = require("../Auth/Auth.repositories")
+const AuthRepository = require("../Auth/Auth.repositories");
+
 
 const BusinessService = {
 createTask: async (tenantId, userId, data) => {
@@ -14,13 +15,13 @@ createTask: async (tenantId, userId, data) => {
     throw new AppError("Assigned user email required", 400);
   }
 
-  // Find assigned user
+  
   const assignedUser = await AuthRepository.findbyemail(email)
   if (!assignedUser) {
     throw new AppError("Assigned user not found", 404);
   }
 
-  // Validate user belongs to tenant
+  
   const isUserInTenant = await BusinessRepository.validateAssignUser(
     tenantId,
     assignedUser.id
@@ -30,7 +31,7 @@ createTask: async (tenantId, userId, data) => {
     throw new AppError("User is not registered in tenant", 403);
   }
 
-  // Create task
+  
   const task = await BusinessRepository.createTask(
     tenantId,
     title,
@@ -46,6 +47,19 @@ createTask: async (tenantId, userId, data) => {
   }
 
   return task;
+},
+fetchTask: async(userid,tenantid)=>{
+  if(!userid) throw new AppError("User id not found",400)
+  if(!tenantid) throw new AppError("tenant id not found",400)
+  const task = await BusinessRepository.fetchTask(userid,tenantid)
+ return task
+},
+deleteTask:async(taskid,tenantid)=>{
+  if(!taskid) throw new AppError("task id not found",400)
+  if(!tenantid) throw new AppError("Tenant id not found",400)
+  const task = await BusinessRepository.deleteTask(taskid,tenantid)
+
+return task;
 }
 
 
