6b5bd923691e7da262858f4cbb317def3f1b7f0e
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-22 11:58:31 +0530

    feat(business):add a router for fetchtask and deletetask

--- src/routers/BusinessRouters.js
+++ src/routers/BusinessRouters.js
@@ -5,6 +5,7 @@ const Authenticate = require("../middleware/Authenticate")
 router.use(Authenticate)
 
 router.post('/:id/createtask',BusinessControllers.createTask)
-
+router.get('/:id/tasks',BusinessControllers.fetchTask)
+router.post('/:id1/:id2/deletetask',BusinessControllers.deleteTask)
 
 module.exports = router
