d7136734f8ef771763f915f30af1e66a1cb69211
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-03-26 11:53:08 +0530

    middleware: replace pool from getpool

--- src/middleware/can.js
+++ src/middleware/can.js
@@ -1,39 +1,53 @@
-const pool = require("../../config/db");
+const { getPool } = require("../../config/db");
 const AppError = require("../utils/AppError");
 
 const can = (permission) => {
   return async (req, res, next) => {
-    const userId = req.user.id;
-    const tenantId = req.params.id;
+    try {
+      const pool = getPool();
 
-    if (!tenantId) {
-      return next(new AppError("Tenant id missing in params", 400));
+      if (!req.user || !req.user.id) {
+        return next(new AppError("Unauthorized", 401));
+      }
+
+      const userId = req.user.id;
+
+      const tenantId =
+        req.params.id || req.body.tenant_id || req.query.tenantId;
+        console.log(userId,tenantId)
+
+      if (!tenantId) {
+        return next(new AppError("Tenant id missing", 400));
+      }
+
+      const query = `
+        SELECT 1
+        FROM user_tenants ut
+        JOIN roles r ON ut.role_id = r.id
+        JOIN role_permissions rp ON rp.role_id = r.id
+        JOIN permissions p ON rp.permission_id = p.id
+        WHERE ut.user_id = $1
+          AND ut.tenant_id = $2
+          AND ut.status = 'active'
+          AND p.name = $3
+        LIMIT 1
+      `;
+
+      const result = await pool.query(query, [
+        userId,
+        tenantId,
+        permission,
+      ]);
+ 
+      if (result.rowCount === 0) {
+        return next(new AppError("Forbidden", 403));
+      }
+
+      next();
+    } catch (err) {
+      console.error("Authorization error:", err);
+      return next(new AppError("Authorization failed", 500));
     }
-    
-    const query = `
-      SELECT 1
-      FROM user_tenants ut
-      JOIN roles r ON ut.role_id = r.id
-      JOIN role_permissions rp ON rp.role_id = r.id
-      JOIN permissions p ON rp.permission_id = p.id
-      WHERE ut.user_id = $1
-        AND ut.tenant_id = $2
-        AND ut.status = 'active'
-        AND p.name = $3
-      LIMIT 1
-    `;
-
-    const result = await pool.query(query, [
-      userId,
-      tenantId,
-      permission,
-    ]);
-    
-    if (result.rowCount === 0) {
-      return next(new AppError("Forbidden", 403));
-    }
-
-    next();
   };
 };
 
