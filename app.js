25a6482a99fe6c06b2b3c96f089519a0100efe5d
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-03-26 11:42:54 +0530

    refactor:add Frontend URL from env

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
+
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json())
 app.use(cookieParser())
 
